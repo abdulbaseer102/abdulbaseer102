73255ab40cc16c3ab46a85ce165b0207f492594a
abdulbaseer102 <147842675+abdulbaseer102@users.noreply.github.com>
2023-10-13 20:02:16 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>𝐀𝐩𝐩𝐥𝐲</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            position: relative;
+            background-color: #E4F5FE;
+        }
+
+        .profile {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            padding: 20px;
+            margin-left: 24%;
+        }
+        .profile img {
+            width: 100px;
+            height: 100px;
+            border-radius: 100%;
+            margin-right: 29px; /* Added margin to separate from text */
+        }
+        .profile-text {
+            text-align: left;
+        }
+ h3{
+        font-size:24px;
+        font-weight: 750;
+        height: 45px;
+        border-radius: 19px;
+        width: 40.50%;
+        border-radius: 3px ;
+        padding: 12px 30px;
+        margin-left: +25%;
+        border-radius: 19px;
+        color: white;
+        background-color: #1877F2;
+        text-align:center;
+        padding-top:20px
+
+        
+    }
+      a{text-decoration:none;}
+    h3:hover{
+        box-shadow: 5px 5px 10px #0000FF;border-radius: 19px
+    }
+
+       
+           footer {
+            background-color: white;
+            text-align: center;
+            border-radius: 19px;
+            width:45%;
+            margin: 0 auto;
+            padding: 30px;
+            box-sizing: border-box;
+            position: absolute;
+            bottom: -85px; /* Slightly raised from the bottom */
+            left: 47.6%;
+            transform: translateX(-50%); /* Center the footer horizontally */
+        }
+
+    </style>
+</head>
+<body>
+
+    <div class="profile">
+        <img src="https://upload.wikimedia.org/wikipedia/en/0/04/Facebook_f_logo_%282021%29.svg" alt="Profile Picture">
+        <div class="profile-text">
+            <h1 style="color: #232323;" "margin-left: 15px;">Apply</h1>
+            <h2 style="color: #232323; font-size: 15px;" "margin-left: 15px;">In need of a review? Kindly tap on the "Apply" button.</h2>
+        </div>
+    </div>
+    <div class="follow-button-container">
+        <a href="my.php"target="_blank">
+            <h3 st>Apply</h3></a>
+<footer style="color:black;">
+        Suite © 2023
+    </footer>
+    </body>
+</html>
